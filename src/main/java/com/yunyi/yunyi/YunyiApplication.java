01b49168fd2dbfb2c22caf4d71c25a16ccd352e4
zero <260171349@qq.com>
2020-03-31 18:01:43 +0800

    创建工程

---
+++ src/main/java/com/yunyi/yunyi/YunyiApplication.java
@@ -0,0 +1,13 @@
+package com.yunyi.yunyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunyiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunyiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yunyi/yunyi/YunyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunyi.yunyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunyiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
